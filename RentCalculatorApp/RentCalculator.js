53a5ccca24faee130b90e5c59c70020ffc257723
erickbickler <erickbickler@gmail.com>
2020-10-30 22:21:10 -0500

    changed to createElement

--- RentCalculatorApp/RentCalculator.js
+++ RentCalculatorApp/RentCalculator.js
@@ -3,20 +3,26 @@ function createForm() {
     var form = document.createAttribute("form");
 }
 
+function textInput(labelText, id, parent) {
+    // create label
+    var newLabel = document.createElement("label");
+    newLabel.setAttribute("for", id);
+    newLabel.appendChild(document.createTextNode("labelText"));
+    parent.appendChild(newLabel);
+
+    // create text input
+    var newInput = document.createElement("input");
+    newInput.setAttribute("type", "text");
+    newInput.setAttribute("name", id);
+    newInput.setAttribute("id", id);
+    parent.appendChild(newInput);
+}
+
 var itemCounter = 0;
 function addNew() {
     var container = document.getElementById("main_form");
 
-    // create label for text input
-    var nameLabelLabel = container.appendChild("input");
-    nameLabelLabel.setAttribute("for", "name_label");
-    nameLabelLabel.appendChild(document.createTextNode("Label"));
+    textInput("Label", "name_label", container);
+    textInput("Total Cost", "total_cost", container);
 
-    // create text input field for the 
-    var nameLabelInput = container.appendChild("input");
-    nameLabelInput.setAttribute("type", "text");
-    nameLabelInput.setAttribute("name", "name_label");
-    nameLabelInput.setAttribute("id", "name_label");
-
-    
 }
