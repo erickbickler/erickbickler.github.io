d05c700cefd0fa8321b3228c747a9aef52c78666
erickbickler <erickbickler@gmail.com>
2021-08-05 11:02:56 -0500

    updated info for projects

--- src/app/Models/projects.ts
+++ src/app/Models/projects.ts
@@ -9,6 +9,10 @@ const TOOLS: Tool[] = [
     {
         "name": "Firebase",
         "imagePath": "./assets/images/tools/Firebase_Logo_Logomark.svg"
+    },
+    {
+      "name": "Django",
+      "imagePath": "./assets/images/tools/django-logo-negative.svg"
     }
 ]
 
@@ -16,7 +20,7 @@ export const PROJECTS: Project[] = [
     {
         title: "Advanced Software Development Class Project",
         subtitle: "The Saucy Hedgehog",
-        description: "Class project for Advanced Software Development. A website built for a restaurant named The Spicy Hedgehog",
+        description: "Class project for Advanced Software Development. A website built for a restaurant named The Spicy Hedgehog. Built with a menu, location list, and daily specials that are automatically generated and refresh daily",
         primaryImage: "./assets/images/projects/saucy_hedgehog/saucy_hedgehog_homepage.PNG",
         secondaryImages: [
           "./assets/images/projects/saucy_hedgehog/saucy_hedgehog_locations.PNG",
@@ -25,7 +29,8 @@ export const PROJECTS: Project[] = [
         ],
         tools: [
           TOOLS.find(tool => tool.name == "Angular")!,
-          TOOLS.find(tool => tool.name == "Firebase")!
+          TOOLS.find(tool => tool.name == "Firebase")!,
+          TOOLS.find(tool => tool.name == "Django")!
         ],
         routingName: "saucy_hedgehog"
       }
